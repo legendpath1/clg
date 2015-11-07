8966f2d2183b9e22b2063139e0c388593c0e09a5
legendpath1 <yan@legendpath.com>
2015-11-07 01:25:43 -0500

    bug fix 3

--- letou_submit.php
+++ letou_submit.php
@@ -16,7 +16,7 @@ $nums=$_POST['data'];
 
 $exe1 = mysql_query("update ssc_member set leftmoney='". ($rowb['leftmoney']-5) ."' where id='". $_SESSION['uid'] ."'");
 $exe2 = mysql_query("update clg_letou set sales='". ($rowa['sales']+5) ."', prize='". ($rowa['prize']+5) ."' where status='1'");
-$exe3 = mysql_query("insert into clg_letourecords set letouid='". $rowa['id'] ."', timestamp='". date("Y-m-d H:i:s") ."', nums='". $nums ."', raise='1', money='5', status='0' ") or  die("数据库修改出错!!!!");
+$exe3 = mysql_query("insert into clg_letourecords set letouid='". $rowa['id'] ."', timestamp='". date("Y-m-d H:i:s") ."', nums='". $nums ."', raise='1', money='5', status='3', username='".$_SESSION['username']."', uid='".$_SESSION['uid']."'") or  die("数据库修改出错!!!!");
 
 echo "<script language=javascript>window.location='./letou.php';</script>";
 exit;
